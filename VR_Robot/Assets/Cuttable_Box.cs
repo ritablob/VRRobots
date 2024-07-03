f21c88dc7d52cf39d3c2a8c8eeb8eb13b05e6263
Xavier Langlands <langlandsx@gmail.com>
2024-07-03 23:19:27 +0200

    Box cutting in

---
+++ VR_Robot/Assets/Cuttable_Box.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+public class Cuttable_Box : MonoBehaviour
+{
+    [SerializeField] private List<Transform> parts;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Box_Cutter") {
+            // Choose a random piece and break it off. 
+            int r = Random.Range(0, parts.Count);
+
+            // Detatch from parent
+            parts[r].transform.parent = null;
+
+            // Enable it's rigidbody and collider
+            parts[r].GetComponent<Rigidbody>().isKinematic = false;
+            parts[r].GetComponent<Collider>().enabled = true;
+            parts[r].GetComponent<XRGrabInteractable>().enabled = true;
+
+            // Remove from list
+            parts.RemoveAt(r);
+
+            if (parts.Count == 0) { Destroy(gameObject); }
+        }
+    }
+}
