37c46fda49d5a1a75d536c8fe806791e1009cdd9
Rita <ritagurgzd@gmail.com>
2024-07-15 15:09:46 +0200

    handwave progress, velocity tracking done, rotation tracking wip

--- VR_Robot/Assets/Scripts/Interaction/HandWaveController.cs
+++ VR_Robot/Assets/Scripts/Interaction/HandWaveController.cs
@@ -1,110 +1,115 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using TMPro;
-using Unity.VisualScripting;
-using Unity.XR.OpenVR;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace Interaction
 {
-    public class HandWaveController : MonoBehaviour
+    public sealed class HandWaveController : MonoBehaviour
     {
-        // TODO: add rotation to the equation
-        
-        public event Action handWaved;
-        public GameObject RightHandAnchor;
-        private Vector3 lastPosition;
+        public Transform leftHandAnchor;
+        public Transform rightHandAnchor;
         public TextMeshProUGUI text;
         public float responseTime = 1.0f;
-        private bool coroutineRunning = false;
         public float movementThreshold = 1.2f;
+        private bool coroutineRunning;
+        private Vector3 lastPositionLeft;
+        private Vector3 lastPositionRight;
 
         private void Start()
         {
-            handWaved += CheckVelocity;
+            coroutineRunning = false;
         }
 
         private void FixedUpdate()
-        {   
-            float distance = Vector3.Distance(lastPosition, RightHandAnchor.transform.position) / Time.deltaTime;
-            //Debug.LogWarning("distance "+distance);
-            if (distance > 1 && !coroutineRunning)
-            {
-                handWaved?.Invoke();
-            }
-            else if (!coroutineRunning)
-            {
-                text.color = Color.red;
-                text.text = "Speed: " +$"{distance:0.0}";
-            }
-            lastPosition = RightHandAnchor.transform.position;
-        }
-        /*
-        * - rotation is facing up
-         * - velocity of hand has an increase
-        * How to do it:
-        * - Quaternion
-         * - Dot product of normalized vectors
-         * https://docs.unity3d.com/ScriptReference/Vector3.Angle.html
-         * 
-         * 
-        */
-        // Start is called before the first frame updat
-
-        // Update is called once per frame
-        private void Update()
         {
-        
+            // if coroutine is not running (i.e. no waving coroutine has started)
+            if (!coroutineRunning)
+            {
+                float velocityLeft = Vector3.Distance(lastPositionLeft, leftHandAnchor.transform.position)/Time.fixedDeltaTime;
+                float velocityRight = Vector3.Distance(lastPositionRight, rightHandAnchor.transform.position)/Time.fixedDeltaTime;
+                
+                //preview text
+                text.color = Color.red;
+                text.text = "Speed left: " + $"{velocityLeft:0.0}" + ", Speed right: " + $"{velocityRight:0.0}";
+                
+                CheckRequirements(velocityLeft, leftHandAnchor);
+                CheckRequirements(velocityRight, rightHandAnchor);
+                
+                lastPositionLeft = leftHandAnchor.position;
+                lastPositionRight = rightHandAnchor.position;
+            }
         }
+
+        public event Action<Transform> handWaved;
 
         private void CheckRotation()
         {
-            
         }
-
-        private void CheckVelocity()
+        
+/// <summary>
+/// Checks whether the immediate requirements are met, then starts the GetAverageVelocity coroutine to check whether
+/// the requirements are being met over a period of time.
+/// </summary>
+/// <param name="velocity">current velocity of an object.</param>
+/// <param name="handTransform">Transform of a hand </param>
+        private void CheckRequirements(float velocity, Transform handTransform)
         {
-            // track velocity every fixed frame for given time
-
-            StartCoroutine(GetAverageVelocity());
-        }
-
-        private IEnumerator GetAverageVelocity()
-        {
-            coroutineRunning = true;
-            //Debug.Log("Called coroutine!");
-            float currentTime = 0f;
-            float cumulativeVelocity = 0f;
-            while (currentTime < responseTime){
-                //Debug.Log("Tracking: "+currentTime);
-                cumulativeVelocity +=
-                    Vector3.Distance(lastPosition, RightHandAnchor.transform.position) / Time.deltaTime;
-                lastPosition = RightHandAnchor.transform.position;
-                // check velocity 
-                currentTime += Time.deltaTime;
-                yield return null;
-            }     
-            //Debug.Log("Done!");
-            float velocity = cumulativeVelocity / currentTime*Time.deltaTime;
-            currentTime = 0;
             if (velocity > movementThreshold)
             {
+                StartCoroutine(GetAverageVelocity(handTransform));
+            }
+        }
+
+/// <summary>
+/// Coroutine to calculate an average velocity over a period of time.
+/// </summary>
+/// <param name="anchorTransform">transform of the hand object</param>
+/// <returns></returns>
+        private IEnumerator GetAverageVelocity(Transform anchorTransform)
+        {
+            coroutineRunning = true;
+            Debug.Log("Called coroutine for "+anchorTransform.gameObject);
+            
+            var currentTime = 0f;
+            var cumulativeVelocity = 0f;
+            var lastPosition = anchorTransform.position;
+            yield return null;
+            // every frame for the duration of the responseTime
+            while (currentTime < responseTime)
+            {
+                // add velocity to cumulativeVelocity 
+                cumulativeVelocity +=
+                    Vector3.Distance(lastPosition, anchorTransform.position);
+                // reset the position 
+                lastPosition = anchorTransform.position;
+                // Add frame length
+                currentTime += Time.fixedDeltaTime;
+                yield return null;
+            }
+
+
+            // calculate average velocity
+            var velocity = cumulativeVelocity / currentTime;
+            Debug.Log("Done! Avg velocity of "+anchorTransform.gameObject+": "+velocity);
+            
+            if (velocity > movementThreshold)
+            {
+                // velocity passed the check 
                 text.color = Color.green;
-                text.text = "Waving! Speed: " +$"{velocity:0.0}";
+                text.text = "Waving! Speed: " + $"{velocity:0.0}";
+                // TODO: check hand rotation 
+                //handWaved.Invoke(anchorTransform);
             }
             else
             {
+                // velocity failed the check
                 text.color = Color.red;
-                text.text = "Speed: " +$"{velocity:0.0}";
+                text.text = "Speed: " + $"{velocity:0.0}";
             }
-            yield return velocity;
+            
             coroutineRunning = false;
-        }
-        protected virtual void OnHandWaved()
-        {
-            handWaved?.Invoke();
+            yield return null;
         }
     }
-}
+ }
