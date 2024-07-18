b4ea7b23757ee92bf11ce15bff271cfeaa56d56f
Rita <ritagurgzd@gmail.com>
2024-07-18 09:05:40 +0200

    update main menu

--- VR_Robot/Assets/Scripts/UI/MainMenuManager.cs
+++ VR_Robot/Assets/Scripts/UI/MainMenuManager.cs
@@ -1,23 +1,40 @@
 using System;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 namespace UI
 {
     public class MainMenuManager : MonoBehaviour
     {
+        public Canvas canvas;
         public GameObject mainMenu;
         public GameObject options;
         public GameObject credits;
-
+        public GameObject head;
+        
         private void Start()
         {
             options.SetActive(false);
             credits.SetActive(false);
-            mainMenu.SetActive(true);
+            //mainMenu.SetActive(true);
+            Invoke(nameof(PositionANdRevealMainMenu), 1f);
         }
 
+        private void PositionANdRevealMainMenu()
+        {
+            // get camera height / 2
+            // get head rotation and make the canvas face it
+            //Vector3 playerPosition = headPositionInpu
+            Vector3 menuPosition = new Vector3(head.transform.position.x, head.transform.position.y,
+                head.transform.position.z + 0.5f);
+            canvas.transform.position = menuPosition;
+            canvas.transform.eulerAngles = new Vector3(0, head.transform.eulerAngles.y,
+                0);
+            mainMenu.SetActive(true);
+        }
         public void StartGamePressed()
         {
             // TODO: level loading pre-scene (fade)
